915791f3d86c4eb2513a4beed4cf5e3f0be4c970
Vasily Sokolov <vasilysokolov@mail.ru>
2022-11-02 18:28:34 +0300

    Добавьте файлы проекта.

---
+++ FunctionTask4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+void evendigits(long&, int*);
+
+int main()
+{
+    int ans = 0;
+    long n = 9223372036854775806;
+    evendigits(n, &ans);
+    std::cout << ans;
+}
+
+void evendigits(long &n, int *ans)
+{
+    std::cout << n << std::endl;
+    std::cout << ans << std::endl;
+}
